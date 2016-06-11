a37b7582cca432f0af2609017060ff1941960235
Christian <chriamue@gmail.com>
2016-06-11 08:49:50 +0200

    refactored save plugin and handler config

--- source/hyphamanager/include/connection/connectionwindow.h
+++ source/hyphamanager/include/connection/connectionwindow.h
@@ -1,62 +1,60 @@
 #ifndef CONNECTIONWINDOW_H
 #define CONNECTIONWINDOW_H
 
-#include <QtWidgets/QWidget>
 #include <QtCore/QList>
-#include <QtWidgets/QTreeWidgetItem>
 #include <QtCore/QMap>
 #include <QtWidgets/QGraphicsScene>
+#include <QtWidgets/QTreeWidgetItem>
+#include <QtWidgets/QWidget>
 #include "../main/instance.h"
 #include "handleritem.h"
 #include "pluginitem.h"
-
 
 namespace Ui {
 class ConnectionWindow;
 }
 
 class ConnectionWindow : public QWidget {
-    Q_OBJECT
+  Q_OBJECT
 
-  public:
-    explicit ConnectionWindow(Instance *instance, QWidget *parent = 0);
-    ~ConnectionWindow();
-    void moveTab(QString name);
-    void savePositions();
-    void saveConfig();
-    void updateDesigner();
-    void setStatusMessageUrl(QString url);
+ public:
+  explicit ConnectionWindow(Instance *instance, QWidget *parent = 0);
+  ~ConnectionWindow();
+  void moveTab(QString name);
+  void savePositions();
+  void saveConfig();
+  void updateDesigner();
+  void setStatusMessageUrl(QString url);
 
-  private:
-    Ui::ConnectionWindow *ui;
+ private:
+  Ui::ConnectionWindow *ui;
 
-  protected:
-    void createPluginsTree();
-    void createPluginsTabs();
-    void createHandlerItems();
-    void updatePluginItems();
-    void loadPositions();
-    void loadPosition(std::string id, QGraphicsItem *item);
+ protected:
+  void createPluginsTree();
+  void createPluginsTabs();
+  void createHandlerItems();
+  void updatePluginItems();
+  void loadPositions();
+  void loadPosition(std::string id, QGraphicsItem *item);
 
-    void savePosition(std::string id, int x, int y);
-    void saveHandlerConfig(std::string id, std::string config);
-    void savePluginConfig(std::string id, std::string config);
+  void savePosition(std::string id, int x, int y);
 
-    void addLines();
+  void addLines();
 
-    Instance *instance;
-    QGraphicsScene *scene;
-    QList<QTreeWidgetItem *> pluginsTreeItems;
-    QMap<QString, QWidget *> pluginTabs;
-    QMap<QString, HandlerItem *> handlerItems;
-    QMap<QString, PluginItem *> pluginItems;
+  Instance *instance;
+  QGraphicsScene *scene;
+  QList<QTreeWidgetItem *> pluginsTreeItems;
+  QMap<QString, QWidget *> pluginTabs;
+  QMap<QString, HandlerItem *> handlerItems;
+  QMap<QString, PluginItem *> pluginItems;
 
-  private slots:
-    void on_pluginsTreeWidget_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
-    void on_connectionsButton_clicked();
-    void on_handlerButton_clicked();
-    void on_pluginButton_clicked();
-    void on_printButton_clicked();
+ private slots:
+  void on_pluginsTreeWidget_currentItemChanged(QTreeWidgetItem *current,
+                                               QTreeWidgetItem *previous);
+  void on_connectionsButton_clicked();
+  void on_handlerButton_clicked();
+  void on_pluginButton_clicked();
+  void on_printButton_clicked();
 };
 
-#endif // CONNECTIONWINDOW_H
+#endif  // CONNECTIONWINDOW_H
